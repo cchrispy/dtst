2001da0010649612654e9e2a3f2480ac32cbb24d
Chris Chiang <cchiang1992@gmail.com>
2017-04-05 17:39:47 -0700

    Tree implementation with searchDF, searchBF, addNode functions

--- src/data/Tree.js
+++ src/data/Tree.js
@@ -17,18 +17,30 @@ class Tree {
     this.size = val ? 1 : 0;
   }
 
-  searchDF(cb) {
+  searchDF(cb, node = this._head) {
     /*
     ** Depth-first traversal to visit the nodes of the tree
     ** and invoke a callback function on each node
     */
+    cb(node);
+    for (let i = 0; i < node.children.length; i++) {
+      searchDF(cb, node.children[i]);
+    }
   }
 
-  searchBF(cb) {
+  searchBF(cb, queue = [this._head]) {
     /*
     ** Breadth-first traversal to visit the nodes of the tree
     ** and invoke a callback function on each node
     */
+    var node = queue.shift();
+    for (let i = 0; i < node.children.length; i++) {
+      queue.push(node.children[i]);
+    }
+    cb(node);
+    if (queue.length) {
+      this.searchBF(cb, queue);
+    }
   }
 
   addNode(val, parent) {
@@ -39,12 +51,17 @@ class Tree {
       ** Assign the node to the root if no root node exists in the tree
       */
       this._head = _node;
-    } else {
+    } else if (parent) {
       /*
       ** Attach the new node to the referenced node (parent -> child)
       */
       _node.parent = parent;
       parent.children.push(_node);
+    } else {
+      /*
+      ** If there is no referenced parent node, throw Error
+      */
+      throw new Error('Need a parent node to add the new node to.');
     }
     /*
     ** Increment the size of the tree.
