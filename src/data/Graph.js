330ae3d41ea97d43a43957aa026d727b65a61e4a
Chris Chiang <cchiang1992@gmail.com>
2017-04-09 16:47:06 -0700

    Instructions for Graph methods

--- src/data/Graph.js
+++ src/data/Graph.js
@@ -7,26 +7,51 @@ class Graph {
     this.vertices = {};
   }
 
+  /*
+  ** Creates a vertex node that contains a value
+  ** The value should be stringified and stored in Graph.vertices
+  ** An edge can be drawn to/from this vertex with addEdge()
+  */
   addVertex(val) {
 
   }
 
+  /*
+  ** Removes the vertex corresponding to the value
+  ** All participating edges to/from this vertex should be removed
+  ** An edge can be removed to/from this vertex with removeEdge()
+  */
   removeVertex(val) {
 
   }
 
+  /*
+  ** Returns the collection of vertices that this vertex links to
+  ** Note: this is a directed graph (edges have a one-way connection)
+  */
   getNeighbors(vertex) {
 
   }
 
+  /*
+  ** Creates an edge that links a vertex to another vertex
+  ** An optional weight can be given to the edge, defaults to 0
+  */
   addEdge(fromNode, toNode, weight) {
 
   }
 
+  /*
+  ** Removes an edge that links a vertex to another vertex
+  */
   removeEdge(fromNode, toNode) {
 
   }
 
+  /*
+  ** Returns the edge that links a vertex to another vertex
+  ** Useful for adjusting the weight of that edge
+  */
   getEdge(fromNode, toNode) {
 
   }
