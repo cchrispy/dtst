602620748bdf517ccb9e42b6de0ac5166a524367
Chris Chiang <cchiang1992@gmail.com>
2017-04-10 18:25:26 -0700

    Document Vertex methods

--- src/data/utils/Vertex.js
+++ src/data/utils/Vertex.js
@@ -3,6 +3,21 @@ class Vertex {
     this.value       = val;
     this.connections = {};
   }
+
+  /*
+  ** Draws a connection between this node to another node
+  */
+  connect(edge) {
+
+  }
+
+  /*
+  ** Returns the collection of vertices that this vertex links to
+  ** Note: this is a directed graph (edges have a one-way connection)
+  */
+  getNeighbors() {
+
+  }
 }
 
 export default Vertex;
