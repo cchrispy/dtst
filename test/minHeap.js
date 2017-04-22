0a39719a2abf0c7e82b8d680a985c74134f9058f
Chris Chiang <cchiang1992@gmail.com>
2017-04-21 18:10:44 -0700

    Tests for min heap methods

--- test/minHeap.js
+++ test/minHeap.js
@@ -20,30 +20,65 @@ describe('Min Heap', () => {
     expect(minHeapSample.searchForIndex).to.be.a('function');
   });
 
-  xdescribe('Min Heap methods', () => {
+  describe('Min Heap methods', () => {
 
     var Heap;
     beforeEach(() => {
 
       Heap = new MinHeap();
 
-      var numbers = [42, 9, 5, 12, 11, 99, 1, 23, 28, 33, 20];
-      numbers.forEach(number => {
-        Heap.insert(number);
-      })
 
     })
 
     it('The root element should be the smallest', () => {
+      expect(Heap.peek()).to.be.undefined;
+
+      var numbers = [42, 9, 5, 12, 11, 99, 1, 23, 28, 33, 20];
+      numbers.forEach(number => {
+        Heap.insert(number);
+      });
+
       expect(Heap.peek()).to.equal(1);
-    })
+      expect(Heap.extract()).to.equal(1);
+      expect(Heap.peek()).to.equal(5);
+
+      Heap.delete(1);
+      expect(Heap.peek()).to.equal(9);
+
+      Heap.insert(0);
+      expect(Heap.peek()).to.equal(0);
+      expect(Heap.size).to.equal(10);
+    });
 
     it('Delete and extract should rearrange the heap accordingly', () => {
 
+      var numbers = [4, 2, 5, 3, 1, 9, 7, 8, 6];
+      numbers.forEach(number => {
+        Heap.insert(number);
+      });
+
+      expect(Heap._storage.slice(1)).to.deep.equal([1, 2, 5, 4, 3, 9, 7, 8, 6]);
+
+      Heap.extract();
+      expect(Heap._storage.slice(1)).to.deep.equal([2, 3, 5, 4, 6, 9, 7, 8]);
+      expect(Heap.delete(6)).to.equal(9);
+      expect(Heap._storage.slice(1)).to.deep.equal([2, 3, 5, 4, 6, 8, 7]);
+      expect(Heap.delete(2)).to.equal(3);
+      expect(Heap._storage.slice(1)).to.deep.equal([2, 4, 5, 7, 6, 8]);
     });
 
     it('Search and SearchForIndex', () => {
 
-    })
+      var numbers = [4, 2, 5, 3, 1, 9, 7, 8, 6];
+      numbers.forEach(number => {
+        Heap.insert(number);
+      });
+
+      expect(Heap.searchForIndex(5)).to.equal(3);
+
+      var store = [];
+      Heap.search(val => store.push(val));
+      expect(store).to.deep.equal([1, 2, 5, 4, 3, 9, 7, 8, 6]);
+    });
   })
 })
