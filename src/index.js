e65f42c0b56b826e57fb68737fc09008697598d6
Chris Chiang <cchiang1992@gmail.com>
2017-04-21 17:18:07 -0700

    Export Min Heap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tree from './data/Tree';
 import Trie from './data/Trie';
-import Graph from './data/Graph';
 import BinarySearchTree from './data/BinarySearchTree';
+import Graph from './data/Graph';
+import MinHeap from './data/MinHeap';
 
-export default { Tree, Trie, Graph, BinarySearchTree }
+export default { Tree, Trie, BinarySearchTree, Graph, MinHeap }
