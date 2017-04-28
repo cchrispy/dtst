f35018e80162ad3872a71ff83b1eaba5fc77bbd4
Chris Chiang <cchiang1992@gmail.com>
2017-04-28 14:45:51 -0700

    Export linked list

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import Trie from './data/Trie';
 import BinarySearchTree from './data/BinarySearchTree';
 import Graph from './data/Graph';
 import MinHeap from './data/MinHeap';
+import LinkedList from './data/LinkedList';
 
 export default { Tree, Trie, BinarySearchTree, Graph, MinHeap }
